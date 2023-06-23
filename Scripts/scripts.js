96d213bc27cc2372affde24e82aa4a39a7b15d94
morten-l-k <kgt837@alumni.ku.dk>
2023-06-23 11:40:04 +0200

    Added folders and content

---
+++ Scripts/scripts.js
@@ -0,0 +1,8 @@
+function changeColor(newColor) {
+    const elem = document.getElementById("changeName");
+    elem.style.color = newColor;
+}
+
+function changeName() {
+    document.getElementById("changeName").innerHTML = "New text";
+}
